2a725c1b304ccd13148a84d60d7cda05f6269f2d
Kamil Orlicki <kamil.orlicki@outlook.com>
2022-01-07 03:02:05 +0100

    Add 2x2 group finding

--- main.cpp
+++ main.cpp
@@ -10,17 +10,70 @@ enum class Value { zero = '0', one = '1', dont_care = 'X' };
 class Map4x4
 {
     std::array<std::array<Value, 4>, 4> kmap;
-    std::array<std::pair<uint8_t, uint8_t>, 4> group{{ {1, 1}, {2, 1}, {1, 2}, {2, 2} }};
+    uint8_t zeroes = 16;
+    std::vector<std::array<std::pair<uint8_t, uint8_t>, 4>> square2x2Groups;
+    std::vector<std::array<std::pair<uint8_t, uint8_t>, 8>> rect4x2Groups;
+
     void setNewIndexes(std::pair<uint8_t, uint8_t>&, int8_t, int8_t);
+    void findSquare2x2Groups();
+    void findRect4x2Groups();
+    void moveGroup(std::array<std::pair<uint8_t, uint8_t>, 4>&, int8_t, int8_t);
+    bool isGroup(std::array<std::pair<uint8_t, uint8_t>, 4>&);
 
 public:
     void printKmap() const;
     void initializeKmap();
-    void moveGroup(int8_t, int8_t);
+    void initializeKmapWith(std::array<std::array<Value, 4>, 4>&);
     void printGroup() const;
+    void printSquare2x2Groups() const;
+    void findGroups();
 };
 
-void Map4x4::moveGroup(int8_t row, int8_t col)
+void Map4x4::findRect4x2Groups()
+{
+    std::array<std::pair<uint8_t, uint8_t>, 12> rect4x2GroupsIndexes {{
+        {0, 0}, {1, 3}, {0, 0}, {3, 1}, {0, 2}, {3, 3},
+        {1, 0}, {2, 2}, {2, 0}, {3, 3}, {0, 1}, {3, 2}
+    }};
+
+    // TODO
+}
+
+void Map4x4::findSquare2x2Groups() 
+{
+    std::array<std::pair<uint8_t, uint8_t>, 4> group{{ 
+        {3, 3}, {0, 3}, {3, 0}, {0, 0} 
+    }};
+
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            if (isGroup(group))
+            {
+                square2x2Groups.push_back(group);
+            }
+            moveGroup(group, 0, 1);
+        }
+        moveGroup(group, 1, 0);
+    }
+}
+
+bool Map4x4::isGroup(std::array<std::pair<uint8_t, uint8_t>, 4>& group)
+{
+    for (const auto& elem : group)
+    {
+        if (kmap[elem.first][elem.second] != Value::one 
+            and kmap[elem.first][elem.second] != Value::dont_care)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Map4x4::moveGroup(std::array<std::pair<uint8_t, uint8_t>, 4>& group, int8_t row, int8_t col)
 {
     row %= 4;
     col %= 4;
@@ -48,13 +101,13 @@ void Map4x4::setNewIndexes(std::pair<uint8_t, uint8_t>& indexes, int8_t row, int
         indexes.second += col; 
 }
 
-void Map4x4::printGroup() const
-{
-    for (auto& indexes : group)
-    {
-        std::cout << "(" << (int)indexes.first << ", " << (int)indexes.second << ")" << "\n";
-    }
-}
+// void Map4x4::printGroup() const
+// {
+//     for (auto& indexes : group)
+//     {
+//         std::cout << "(" << (int)indexes.first << ", " << (int)indexes.second << ")" << "\n";
+//     }
+// }
 
 void Map4x4::printKmap() const
 {
@@ -62,7 +115,19 @@ void Map4x4::printKmap() const
     {
         for (const auto& elem : row)
         {
-            std::cout << static_cast<char>(elem);
+            std::cout << static_cast<char>(elem) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Map4x4::printSquare2x2Groups() const
+{
+    for (const auto& group : square2x2Groups)
+    {
+        for (const auto& elem : group)
+        {
+            std::cout << "(" << (int)elem.first << ", " << (int)elem.second << ")" << " ";
         }
         std::cout << std::endl;
     }
@@ -79,36 +144,51 @@ void Map4x4::initializeKmap()
     }
 }
 
+void Map4x4::initializeKmapWith(std::array<std::array<Value, 4>, 4>& kmap)
+{
+    this->kmap = kmap;
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    // sf::CircleShape shape(100.f);
+    // shape.setFillColor(sf::Color::Green);
     
 
-    Map4x4 kmap;
-    kmap.initializeKmap();
-    kmap.printKmap();
-    kmap.printGroup();
-    kmap.moveGroup(-2, -2);
-    kmap.printGroup();
-    std::cout << -1 % 4 << std::endl;
+    std::array<std::array<Value, 4>, 4> testKmap = {{
+        {Value::zero, Value::one, Value::one, Value::zero},
+        {Value::one, Value::zero, Value::zero, Value::one},
+        {Value::one, Value::zero, Value::zero, Value::one},
+        {Value::zero, Value::one, Value::one, Value::zero}
+    }};
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::MouseButtonPressed)
-                std::cout << "Test" << std::endl;
-        }
+    Map4x4 kmapObject;
+    kmapObject.initializeKmapWith(testKmap);
+    kmapObject.printKmap();
+    // kmapObject.findSquare2x2Groups();
+    // kmapObject.printSquare2x2Groups();
+    // kmap.printKmap();
+    // kmap.printGroup();
+    // kmap.moveGroup(-2, -2);
+    // kmap.printGroup();
+    // std::cout << -1 % 4 << std::endl;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    // while (window.isOpen())
+    // {
+    //     sf::Event event;
+    //     while (window.pollEvent(event))
+    //     {
+    //         if (event.type == sf::Event::Closed)
+    //             window.close();
+    //         if (event.type == sf::Event::MouseButtonPressed)
+    //             std::cout << "Test" << std::endl;
+    //     }
+
+    //     window.clear();
+    //     window.draw(shape);
+    //     window.display();
+    // }
 
     return 0;
 }
